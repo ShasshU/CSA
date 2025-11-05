d3141ca8642fe83ad92f2731f6a86a973564cb3f
ShasshU <143851930+ShasshU@users.noreply.github.com>
2025-11-05 13:30:31 -0800

    added else if into Sdivisible number

--- SDivisibleNumber.java
+++ SDivisibleNumber.java
@@ -1,32 +1,21 @@
 public class SDivisibleNumber {
     public static void main(String[] args) {
-        int number = 0;
-        boolean found = false;
 
         for (int i = 1; i <= 10000; i++) {
-            if (i % 1 == 0 &&
-                    i % 2 == 0 &&
-                    i % 3 == 0 &&
-                    i % 4 == 0 &&
-                    i % 5 == 0 &&
-                    i % 6 == 0 &&
-                    i % 7 == 0 &&
-                    i % 8 == 0 &&
-                    i % 9 == 0 &&
-                    i % 10 == 0) {
 
-                number = i;
-                found = true;
-                break;
-            } else {
+            if (i % 2 != 0 || i % 3 != 0 || i % 4 != 0 || i % 5 != 0) {
                 continue;
-            }
-        }
 
-        if (found) {
-            System.out.println("The smallest number evenly divisible by 1-10 is: " + number);
-        } else {
-            System.out.println("Number not found in range. Try increasing the limit.");
+            } else if (i % 6 != 0 || i % 7 != 0 || i % 8 != 0) {
+                continue;
+
+            } else if (i % 9 != 0 || i % 10 != 0) {
+                continue;
+
+            } else {
+                System.out.println("The smallest number evenly divisible by 1-10 is: " + i);
+                break;
+            }
         }
     }
 }
