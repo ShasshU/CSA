a4ba7b78ec3b9f8bf78a284ccc6c422437277f78
ShasshU <143851930+ShasshU@users.noreply.github.com>
2026-01-21 13:20:48 -0800

    updated list algortihm with comments

--- ListAlgorithm.java
+++ ListAlgorithm.java
@@ -24,20 +24,20 @@ class ListAlgorithm {
 
   // write methods
   public static void printPairs(ArrayList<String> arrayList) {
-    for (int i = 0; i < arrayList.size(); i += 2) {
-      System.out.println(arrayList.get(i) + " " + arrayList.get(i + 1));
+    for (int i = 0; i < arrayList.size(); i += 2) { //starts at the first word and skips two 
+      System.out.println(arrayList.get(i) + " " + arrayList.get(i + 1)); //adds the words together with a space in between
     }
 
   }
 
   public static void socal(ArrayList<String> arrayList) {
-    for (int i = 2; i <= arrayList.size(); i += 3) {
+    for (int i = 2; i <= arrayList.size(); i += 3) { //starts at the second word and skips 3
       arrayList.add(i, "like");
     }
   }
 
   public static void moveRight(ArrayList<String> arrayList) {
-    String last = arrayList.remove(arrayList.size() - 1);
-    arrayList.add(0, last);
+    String last = arrayList.remove(arrayList.size() - 1); //stores the last word and removes it
+    arrayList.add(0, last); //readds the last word
   }
 }
